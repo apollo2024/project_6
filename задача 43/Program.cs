bc4e32829252246670d1c02a39d277f4b6753a12
muhammadkodir <muhammadkodyra@email.com>
2022-12-04 17:56:32 +0500

    final one

---
+++ задача 43/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых,
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 
+// задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+Console.Write("Введите k1: ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите b1: ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите k2: ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите b2: ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+
+double temp_x = GetPoint(k1, b1, k2, b2);
+System.Console.WriteLine(GetPoint(k1, b1, k2, b2));
+System.Console.WriteLine(Gety(k1, b1, k2, b2));
+double GetPoint (double k1, double b1, double k2, double b2)
+{
+    double x = (b2 - b1) / (k1 - k2);
+    return x;
+}
+double Gety (double k1, double b1, double k2, double b2)
+{
+    double y = k2 * temp_x + b2;
+    return y;
+}
